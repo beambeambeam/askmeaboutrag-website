dfdb4d5e5205a97e0ceb072423f088965e70db04
beam <supawitmarayat@gmail.com>
2024-09-14 20:42:42 +0700

    feat: add theme

---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,17 @@
+import { Moon, Sun } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { useTheme } from '@/components/theme-provider'
+
+export function ModeToggle() {
+  const { setTheme, theme } = useTheme()
+
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+      {theme === 'dark' ? <Sun /> : <Moon />}
+    </Button>
+  )
+}
